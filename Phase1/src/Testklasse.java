e6485498d43e13a81cb9a0ed7ffd14ba6b1a45ee
Ngoc-Anh <Ngoc-Anh@Ngoc-Anh-PC>
2013-04-06 22:17:08 +0200

    test1

--- Phase1/src/Testklasse.java
+++ Phase1/src/Testklasse.java
@@ -6,7 +6,7 @@ public class Testklasse {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hallo WBA2");
+		System.out.println("Hallo WBA2!");
 
 	}
 
